16542bfa6dc44f88751e4c32d5fe9a7424882e18
exapisde25 <die321d.21@gmail.com>
2025-10-03 17:57:49 -0300

    feat(paticles): particles added in home.ts

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, effect } from '@angular/core';
 import { NavigationSection } from './navigation-section/navigation-section';
 import { HeroSection } from './hero-section/hero-section';
 import { AboutSection } from './about-section/about-section';
@@ -6,6 +6,11 @@ import { ExperienceSection } from './experience-section/experience-section';
 import { EducationSection } from './education-section/education-section';
 import { SkillsSection } from './skills-section/skills-section';
 import { ContactSection } from './contact-section/contact-section';
+import { getParticlesConfig } from './particles-config';
+import { ThemeService } from '../theme.service';
+
+
+declare let particlesJS: any;
 
 @Component({
   selector: 'app-home',
@@ -17,11 +22,28 @@ import { ContactSection } from './contact-section/contact-section';
     ExperienceSection,
     EducationSection,
     SkillsSection,
-    ContactSection
+    ContactSection,
   ],
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrl: './home.scss',
 })
-export class Home {
+export class Home implements OnInit {
+  private themeService = inject(ThemeService);
 
+  constructor() {
+    // React to theme changes
+    effect(() => {
+      const isDarkMode = this.themeService.isDarkMode();
+      this.invokeParticles(isDarkMode);
+    });
+  }
+
+  ngOnInit(): void {
+    this.invokeParticles(this.themeService.isDarkMode());
+  }
+
+  public invokeParticles(isDarkMode: boolean): void {
+    const config = getParticlesConfig(isDarkMode);
+    particlesJS('particles-js', config, function () {});
+  }
 }

78f77abb775209557a031ecac904bd0e3c4d4543
exapisde25 <die321d.21@gmail.com>
2025-10-02 21:08:12 -0300

    feat(experience): experience section added

---
+++ src/app/home/experience-section/experience-section.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experience-section',
+  imports: [],
+  templateUrl: './experience-section.html',
+  styleUrl: './experience-section.scss'
+})
+export class ExperienceSection {
+
+}
